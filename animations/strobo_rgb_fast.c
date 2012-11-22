c9ed8effe730dbe2684d9d7ad18b9485c757c3a4
sebseb7 <github@seb.exse.net>
2012-11-23 00:07:48 +0100

    new animations

---
+++ animations/strobo_rgb_fast.c
@@ -0,0 +1,46 @@
+#include <stdlib.h>
+#include "main.h"
+#include <stdio.h>
+#include <math.h>
+#include "libs/math.h"
+
+static uint16_t b = 0;
+
+static uint8_t tick(void) {
+
+	int x;
+
+	if(b==0)
+		for(x=0;x<LED_WIDTH;x++)
+			setLedX(x,255,0,0);
+	if(b==1)
+		for(x=0;x<LED_WIDTH;x++)
+			setLedX(x,0,255,0);
+	if(b==2)
+		for(x=0;x<LED_WIDTH;x++)
+			setLedX(x,0,0,255);
+	
+	b++;
+	if(b==3)
+		b=0;
+	return 0;
+}
+
+static void init(void)
+{
+	b = 0;
+}
+static void deinit(void)
+{
+}
+
+
+
+static void constructor(void) CONSTRUCTOR_ATTRIBUTES
+void constructor(void) {
+	registerAnimation(init,tick,deinit, 6, 6000);
+}
+
+
+
+
---
+++ animations/white.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include "main.h"
+
+
+static uint8_t tick(void) {
+
+	for(int x=0;x<LED_WIDTH;x++)
+		setLedX(x,255,255,255);
+	return 0;
+}
+
+static void init(void)
+{
+}
+static void deinit(void)
+{
+}
+
+
+
+static void constructor(void) CONSTRUCTOR_ATTRIBUTES
+void constructor(void) {
+	registerAnimation(init,tick,deinit, 29, 600);
+}
+
+
+
+
