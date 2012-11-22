7a34f3c9f76aa1f1f63f8ea4eab1b6ce5966f56a
sebseb7 <github@seb.exse.net>
2012-11-22 23:09:46 +0100

    new "animation" swtich_rgb

---
+++ animations/switch_rgb.c
@@ -0,0 +1,53 @@
+#include <stdlib.h>
+#include "main.h"
+#include <stdio.h>
+#include <math.h>
+#include "libs/math.h"
+
+static uint16_t a = 0;
+static uint16_t b = 0;
+
+static uint8_t tick(void) {
+
+	int x;
+
+	if(b==0)
+		for(x=0;x<LED_WIDTH;x++)
+			setLedX(x,255,0,0);
+	if(b==1)
+		for(x=0;x<LED_WIDTH;x++)
+			setLedX(x,0,255,0);
+	if(b==2)
+		for(x=0;x<LED_WIDTH;x++)
+			setLedX(x,0,0,255);
+	
+	a+=1;
+	if(a==100)
+	{
+		a=0;
+		b++;
+		if(b==3)
+			b=0;
+	}
+	return 0;
+}
+
+static void init(void)
+{
+	a = 0;
+	b = 0;
+}
+static void deinit(void)
+{
+}
+
+
+
+static void constructor(void) CONSTRUCTOR_ATTRIBUTES
+void constructor(void) {
+	registerAnimation(init,tick,deinit, 4, 600);
+}
+
+
+
+
